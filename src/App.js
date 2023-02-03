b68b94fa4032e63b0c15e744d25d6433dc27317f
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 02:33:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h3>ToggleState Hook</h3>
+      <h3>Toggle State Custom Hook</h3>
     </div>
   );
 }
