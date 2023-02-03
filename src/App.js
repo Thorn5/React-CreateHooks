4ee35c8d0e17a3511ee6774f1bbb9fdf9230294c
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 01:16:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Main Branch, climb somewhere else!</h1>
+    </div>
+  );
+};
+
+export default App;
