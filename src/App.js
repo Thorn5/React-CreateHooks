6bb80ce834704ecdf709959d91ed307b113e560d
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 01:30:08 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3>ToggleState Hook</h3>
+    </div>
+  );
+}
---
+++ src/CustomHook/UseToggleState.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function useToggleState(impToggleState, impDesiredTogState) {
+    const [toggleState, setToggleState] = useState(impToggleState);
+    const [desiredTogState, setDesiredTogState] = useState(impDesiredTogState)    
+    if (typeof desiredTogState === 'boolean' && desiredTogState !== null) {
+        setToggleStatus(desiredTogState);
+    } else {
+        if (typeof toggleState === 'boolean' && toggleState !== null) {
+            setToggleStatus(!toggleState);
+        } else {
+            alert("No toggle state provided");
+        }
+    }
+    console.log(toggleState);
+    return (
+        toggleState
+    )
+}
+
+export default useToggleState
