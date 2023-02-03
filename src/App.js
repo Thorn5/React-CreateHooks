629c8f7bdca8f057d3b6f62201326bc54f929974
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 02:33:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
   return (
     <div className="App">
-     <h3>ToggleState Hook</h3>
+     <h3>Fetch Data Custom Hook</h3>
       {data && data.data.map(catFact => (
         <p key={catFact._id} {...catFact}>{catFact.fact}</p>
         ))}
