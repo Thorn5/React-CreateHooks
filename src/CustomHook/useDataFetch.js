8f0aaf19a74bbb0c8283234f099463d9c2f030fa
Tryg <trygvehorn@gmail.com>
2023-02-03 10:20:52 +0000

    Update useDataFetch.js

--- src/CustomHook/useDataFetch.js
+++ src/CustomHook/useDataFetch.js
@@ -3,27 +3,49 @@
 import { useState, useEffect } from "react";
 
 const useDataFetch = (fullUrl) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setLoading(true);
-        try {
-            fetch(fullUrl)
-            .then(response => response.json())
-            .then(jsonResponse => {
-                    setData(jsonResponse);
-                    // console.log(data);
-                    // console.log(data.data[0].fact);
-                })
-        } catch (err) {
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-    }, [fullUrl]);
-    
-    return { data, loading, error };
+  useEffect(() => {
+    setLoading(true);
+    try {
+      fetch(fullUrl)
+        .then((response) => response.json())
+        .then((jsonResponse) => {
+          setData(jsonResponse);
+          // console.log(data);
+          // console.log(data.data[0].fact);
+        });
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [fullUrl]);
+
+  return { data, loading, error };
 };
+
+//  Working Version
+//     useEffect(() => {
+//         setLoading(true);
+//         try {
+//             fetch(fullUrl)
+//             .then(response => response.json())
+//             .then(jsonResponse => {
+//                     setData(jsonResponse);
+//                     // console.log(data);
+//                     // console.log(data.data[0].fact);
+//                 })
+//         } catch (err) {
+//             setError(err);
+//         } finally {
+//             setLoading(false);
+//         }
+//     }, [fullUrl]);
+
+//     return { data, loading, error };
+// };
+
 export default useDataFetch;
