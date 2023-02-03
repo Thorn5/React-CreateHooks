9825347e1d107df53bdbe2997eadeafecb9cc7f2
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-03 22:10:25 +0100

    Update useDataFetch.js

--- src/CustomHook/useDataFetch.js
+++ src/CustomHook/useDataFetch.js
@@ -7,20 +7,22 @@ const useDataFetch = (fullUrl) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  // Adapted from Nuria's version
   useEffect(() => {
     setLoading(true);
     const fetchData = async () => {
-    try {
-      const response = await fetch(fullUrl)
+      try {
+        const response = await fetch(fullUrl)
         const data = await response.json();
         setData(data)
-          // console.log(data);
-          // console.log(data.data[0].fact);
-        
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
+        // console.log(data);
+        // console.log(data.data[0].fact);
+
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
     }
     fetchData()
   }, [fullUrl]);
@@ -28,7 +30,10 @@ const useDataFetch = (fullUrl) => {
   return { data, loading, error };
 };
 
-//  Working Version
+export default useDataFetch;
+
+
+//  My version - works
 //     useEffect(() => {
 //         setLoading(true);
 //         try {
@@ -49,4 +54,3 @@ const useDataFetch = (fullUrl) => {
 //     return { data, loading, error };
 // };
 
-export default useDataFetch;
